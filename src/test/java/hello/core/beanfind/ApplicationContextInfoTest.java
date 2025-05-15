80a7b03918d67138d932aa158a572890b7e9f288
LeeTaeSung0628 <xotjd794613@naver.com>
2025-05-15 22:58:27 +0900

    feat: mac용 branch 업로드

---
+++ src/test/java/hello/core/beanfind/ApplicationContextInfoTest.java
@@ -0,0 +1,9 @@
+package hello.core.beanfind;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextInfoTest {
+
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+}
